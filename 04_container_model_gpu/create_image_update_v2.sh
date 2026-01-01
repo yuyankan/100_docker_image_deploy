4a2f5fbd81b3697ef5920eb719188caa29c04926
yuyankan <89712442+yuyankan@users.noreply.github.com>
2026-01-01 18:02:44 +0800

    Add files via upload

---
+++ 04_container_model_gpu/create_image_update_v2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 定义代理地址
+# 将 "http://gateway.zscalertwo.net:10090" 替换为你的实际代理地址
+# 如果你的代理需要用户名和密码，格式为 "http://username:password@proxy.address:port"
+PROXY_ADDRESS="http://gateway.zscalertwo.net:10090"
+
+# 构建 Docker 镜像并打上标签
+# -t: 给镜像命名和版本号
+# -f: 指定 Dockerfile 的路径
+# .: 构建上下文，代表当前目录
+# --build-arg: 将代理地址作为参数传递给 Dockerfile
+
+echo "Starting Docker image build for model_models_gpu_common:202511..."
+
+docker build --no-cache \
+  --build-arg http_proxy="${PROXY_ADDRESS}" \
+  --build-arg https_proxy="${PROXY_ADDRESS}" \
+  -t models_gpu_common_update_v2:202511 \
+  -f Dockerfile_update_v2 \
+  .
+
+# 检查构建是否成功
+if [ $? -eq 0 ]; then
+  echo "✅ Docker image built successfully."
+else
+  echo "❌ Docker image build failed."
+fi
---
+++ 04_container_model_gpu/readme.txt
@@ -0,0 +1,5 @@
+#1. creat basic image for models
+files:
+Dockerfile
+Dockerfile_update
+#model-compose.yml
